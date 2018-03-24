e67ebbbed33b8165afbfc487db37f4614c4ab817
Hamdi Alperen Çetin <hamdialperencetin@gmail.com>
2018-03-24 12:52:38 +0300

    report format and active connections
    added active tcp/udp connections and collected all reports in a txt file

--- main.ps1
+++ main.ps1
@@ -1,36 +1,66 @@
 <#
 
-This script collects volatile informations in Windows OS and print them to files shown as below.
+This script collects volatile informations in Windows OS and print them to file
+named "Windows_Volatile_Information_Report_<Timestamp>" with the headers below.
 
-System Processes                ->    system_processes.csv
-Running Services                ->    running_services.csv
-DNS Client Cache                ->    dns_client_cache.csv
-ARP Cache                       ->    arp_cache.csv
-Clipboard Information           ->    clipboard.txt
-Currently Visible Networks      ->    networks.txt
+System Processes
+Running Services
+DNS Client Cache
+ARP Cache
+Clipboard Information
+Visible Networks
+Active TCP/UDP Connections
 
 #>
 
+$Timestamp = Get-Date -UFormat "%Y%m%d_%H%M%S_%Z"
+$OutputFileName = "Windows_Volatile_Information_Report_" + $Timestamp + ".txt"
 
-#Prints green colored text to the host
-function Print-Info($Text) {Write-Host -ForegroundColor GREEN $Text}
+#Prints text to the host as green colored 
+function Print-Host-Info($Text) {Write-Host -ForegroundColor GREEN $Text}
 
-Print-Info "...Starting Volatile Informations Script..."
+#Appends text to output file defined above
+function Append-File($Text) {$Text.ToUpper() | Out-File -Append $OutputFileName}
 
-Print-Info "Getting System Processes"
-Get-Process | Export-Csv system_processes.csv
+Print-Host-Info "...Starting Volatile Informations Script..."
 
-Print-Info "Getting Running Services"
-Get-Service | Where-Object {$_.Status -eq "Running"} | Export-Csv running_services.csv
+Append-File("WINDOWS VOLATILE INFORMATION REPORT`n`n")
 
-Print-Info "Getting DNS Client Cache"
-Get-DnsClientCache | Export-Csv dns_client_cache.csv
+Print-Host-Info "Getting System Processes"
+Append-File("SYSTEM PROCESSES")
+Get-Process | Format-Table -AutoSize -Wrap | Out-File -Append $OutputFileName
 
-Print-Info "Getting ARP Cache"
-Get-NetNeighbor | Export-Csv arp_cache.csv
+Print-Host-Info "Getting Running Services"
+Append-File("RUNNING SERVICES")
+Get-Service | Where-Object {$_.Status -eq "Running"} | Format-Table -AutoSize -Wrap | 
+    Out-File -Append $OutputFileName
 
-Print-Info "Getting Clipboard Information"
-Get-Clipboard | Out-File clipboard.txt
+Print-Host-Info "Getting DNS Client Cache"
+Append-File("DNS CLIENT CACHE")
+Get-DnsClientCache | Format-Table -AutoSize -Wrap | Out-File -Append $OutputFileName
 
-Print-Info "Getting Currently Visible Networks"
-netsh wlan show networks | Out-File networks.txt
+Print-Host-Info "Getting ARP Cache"
+Append-File("ARP CACHE")
+Get-NetNeighbor | Format-Table -AutoSize -Wrap | Out-File -Append $OutputFileName
+
+Print-Host-Info "Getting Clipboard Information"
+Append-File("CLIPBOARD INFORMATION`n")
+Get-Clipboard | Out-File -Append $OutputFileName
+Append-File("`n")
+
+Print-Host-Info "Getting Visible Networks"
+Append-File("VISIBLE NETWORKS")
+netsh wlan show networks | Out-File -Append $OutputFileName
+
+Append-File("ACTIVE TCP/UDP CONNECTIONS")
+Try
+{
+    Print-Host-Info "Getting Active TCP/UDP Connections with Application Names"
+    netstat -anob | Out-File -Append $OutputFileName
+}
+Catch # If there is no permission for -b parameter
+{
+    Write-Host -ForegroundColor RED "FAILED. Most probably, you don't have the permission."
+    Print-Host-Info "Getting Active TCP/UDP Connections"
+    netstat -ano | Out-File -Append $OutputFileName
+}
